a612d821815fb6f1bb57473dee2fc56b046ffe6f
hatbann <0urloveis_great@naver.com>
2022-09-30 10:55:00 +0900

    220930

--- src/routes/SelectGenre.js
+++ src/routes/SelectGenre.js
@@ -1,23 +1,23 @@
 import React from 'react';
-import { Link, useNavigate } from "react-router-dom";
+import { Link, useNavigate } from 'react-router-dom';
 
 const SelectGenre = () => {
-    const navigate = useNavigate();
+  const navigate = useNavigate();
 
-    const onSubmit= (event)=>{
-        event.preventDefault();
-        navigate('/dogsbooks');
-    }
-    return (
-        <div>
-            장르선택창
-            <form>
-                <input type="submit" value={"완료"} onClick={onSubmit}>
-
-                </input>
-            </form>
-        </div>
-    );
+  const onSubmit = (event) => {
+    event.preventDefault();
+    navigate('/dogsbooks');
+  };
+  return (
+    <div>
+      <div>
+        <h1>장르선택하기</h1>
+      </div>
+      <form>
+        <input type="submit" value={'완료'} onClick={onSubmit}></input>
+      </form>
+    </div>
+  );
 };
 
 export default SelectGenre;
