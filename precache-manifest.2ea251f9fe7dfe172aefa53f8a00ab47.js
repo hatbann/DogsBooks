cd2b4198eaf967f7f7ce5832bb9863c1b0f7115e
hatbann <0urloveis_great@naver.com>
2022-10-11 13:18:22 +0900

    Updates

---
+++ precache-manifest.2ea251f9fe7dfe172aefa53f8a00ab47.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = [
+  {
+    "revision": "d1a6f78d3132d59c70ca",
+    "url": "/dogsbooks/static/css/main.a6c63a60.chunk.css"
+  },
+  {
+    "revision": "d1a6f78d3132d59c70ca",
+    "url": "/dogsbooks/static/js/main.d1a6f78d.chunk.js"
+  },
+  {
+    "revision": "1eba3af3cb5e793df15f",
+    "url": "/dogsbooks/static/css/1.171a992a.chunk.css"
+  },
+  {
+    "revision": "1eba3af3cb5e793df15f",
+    "url": "/dogsbooks/static/js/1.1eba3af3.chunk.js"
+  },
+  {
+    "revision": "1d20c264f95f7232e559",
+    "url": "/dogsbooks/static/js/runtime~main.1d20c264.js"
+  },
+  {
+    "revision": "2ba70a6085a7f0c785995b8ac8e2753f",
+    "url": "/dogsbooks/static/media/img1.2ba70a60.png"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/dogsbooks/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/dogsbooks/static/media/slick.f97e3bbf.svg"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/dogsbooks/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/dogsbooks/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "68e1a2e51150093acb8588cb0f6b7dce",
+    "url": "/dogsbooks/static/media/titledog.68e1a2e5.png"
+  },
+  {
+    "revision": "c690ccaf58c8e3d804eb91da7ba0ea03",
+    "url": "/dogsbooks/static/media/profile11.c690ccaf.jpg"
+  },
+  {
+    "revision": "83fd6250ea83141319ee89f551bc8fb5",
+    "url": "/dogsbooks/static/media/profile12.83fd6250.png"
+  },
+  {
+    "revision": "66b8161edb118d19e739568075b52450",
+    "url": "/dogsbooks/static/media/Happiness-Sans-Regular.66b8161e.ttf"
+  },
+  {
+    "revision": "008a869bc9c3d32ebcb481dc3388166e",
+    "url": "/dogsbooks/static/media/Happiness-Sans-Title.008a869b.ttf"
+  },
+  {
+    "revision": "fbbf26c8bdeb5e1131bf913653bd806c",
+    "url": "/dogsbooks/index.html"
+  }
+];
