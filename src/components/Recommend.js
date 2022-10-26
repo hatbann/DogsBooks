4d867eaead3e9c37344912c49a45ed60a74c14b8
chaeyeon LEE <98521882+colorkite10@users.noreply.github.com>
2022-10-26 21:53:39 +0900

    [추천알고리즘] 최댓값 구하기 하는 중

--- src/components/Recommend.js
+++ src/components/Recommend.js
@@ -1,25 +1,42 @@
 import { dbService } from "../fbase";
+import { getAuth } from "firebase/auth";
 import {
   collection,
   addDoc,
-  getDocs,
+  getDoc,
   query,
   onSnapshot,
+  DocumentSnapshot,
+  where,
+  getDocs,
+  doc,
+  orderBy,
 } from "firebase/firestore";
 import React, { useEffect, useState } from "react";
 
 const Recommend = () => {
+  const [genreArr, setGenreArr] = useState([]);
   const [genrePoint, setGenrePoint] = useState([]);
-  const userRef = doc(dbService, "UserInfo", `${auth.currentUser.uid}`);
+
+  const auth = getAuth();
 
   useEffect(() => {
-    const q = query(doc(userRef));
-    onSnapshot(q, (snapshot) => {
-      const genreArr = snapshot.docs.map((document) => ({
-        id: document.id,
-        ...document.data(),
-      }));
-      setGenrePoint(genreArr);
-    });
-  }, []);
+    async function fetchData() {
+      const q = query(doc(dbService, "UserInfo", `${auth.currentUser.uid}`));
+      const genreArr = await getDoc(q);
+      let tempGenreArr = genreArr.data();
+      setGenreArr(tempGenreArr);
+    }
+    fetchData();
+  });
+  console.log("genreArr: ", genreArr);
+
+  let values = Object.values(genreArr);
+  console.log("values: ", values);
+  let maxValues = Math.max(...values);
+  console.log("Max: ", maxValues);
+  let key = Object.keys(genreArr);
+  console.log("key: ", key);
 };
+
+export default Recommend;
